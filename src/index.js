521233bd60eec1c15808cea09cc64f7640cb5e6d
Gennadiy <borchuga@gmail.com>
2020-11-02 23:18:45 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import './styles.css';
+const debounce = require('lodash.debounce');
+import countryListTemplate from './handlebars/country-list.hbs';
+import error from './js/pnotyfy/pnotyfy.js'
+import personalCountryTemplate from './handlebars/personal-country-marckup.hbs'
+
+const refs = {
+    searchForm: document.querySelector('.js-searh-form'),
+    counrtySection: document.querySelector('.country-wrapper'),
+    input: document.querySelector('.oop')
+};
+
+
+
+refs.searchForm.addEventListener('input',debounce(onSearch,1000));
+    
+
+function onSearch(e) {
+    e.preventDefault();
+    const searchQuery = e.target.value;
+    getCountryByName(searchQuery)
+    .then(marckupVariablesHandler(searchQuery))
+    // .catch(errors=>console.log(error))
+    };
+
+function marckupVariablesHandler(searchQuery) {
+    if (searchQuery==='') {
+        countryEmptyListMarckup();
+    } else if (searchQuery.length === 1) {
+        personalCountry()
+    } else if (searchQuery.length > 1 && searchQuery.length <= 10) {
+        countryListMarckup() 
+    } else {error}
+    
+}
+
+
+function countryListMarckup(country) {
+    const marckup = countryListTemplate({ country });
+    refs.counrtySection.innerHTML = marckup;
+}  
+
+function countryEmptyListMarckup () {
+    refs.counrtySection.innerHTML = '';
+}
+
+function getCountryByName (url) {
+   return fetch(`https://restcountries.eu/rest/v2/name/${url}`)
+    .then(responce =>{ return responce.json()})
+}
+
+function personalCountry(country) {
+    const marckup = personalCountryTemplate({ country });
+    refs.counrtySection.innerHTML = marckup;
+}
